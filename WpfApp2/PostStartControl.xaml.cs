955dfb9680d6730a1626216f1ca5e7fa762a60fa
Mads Mikkel Rasmussen <mara@aspit.dk>
2020-04-22 10:55:21 +0200

    Add project files.

---
+++ WpfApp2/PostStartControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for PostStartControl.xaml
+    /// </summary>
+    public partial class PostStartControl: UserControl
+    {
+        public PostStartControl()
+        {
+            InitializeComponent();
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mw = MainWindow.Instance;
+            mw.MainContentControl.Content = new StartControl();
+        }
+    }
+}
